34b12fe72f7eaeef9a760c33b3c9890c76b76dde
newtmex <brightomex@gmail.com>
2025-05-22 13:46:45 +0100

    Code cleanup for reorg

--- components/AppKitButton.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const AppKitButton = () => {
-	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-	// @ts-ignore
-	return <appkit-button id="wagmi-button" />;
-};
-
-export default AppKitButton;
